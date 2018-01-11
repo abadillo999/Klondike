71dec2dbf028e6ef01f5c322e4af4fe2a3358d08
abadillo999 <alejandro.gbadillo@gmail.com>
2018-01-11 17:44:45 +0100

    Sycronization Commit

---
+++ cmake-build-debug/CMakeFiles/Solitario.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/CelebrateController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/CelebrateController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/Command/CommandBroker.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/Command/CommandBroker.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/Command/CommandMenu.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/Command/CommandMenu.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/Controller.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/Controller.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/EndController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/EndController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/GameController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/GameController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/Logic.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/Logic.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/Menu/CommandController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/Menu/CommandController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/Menu/ExitController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/Menu/ExitController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/Menu/FlipController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/Menu/FlipController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/Menu/MenuController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/Menu/MenuController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/Menu/MoveController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/Menu/MoveController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/Menu/RedoController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/Menu/RedoController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/Menu/UndoController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/Menu/UndoController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/OperationController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/OperationController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Controllers/StartController.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Controllers/StartController.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Klondike.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Klondike.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Models/Board.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Models/Board.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Models/Card.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Models/Card.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Models/Deck.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Models/Deck.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Models/Foundation.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Models/Foundation.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Models/Game.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Models/Game.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Models/Pile.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Models/Pile.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Models/Queue.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Models/Queue.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Models/Stack.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Models/Stack.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Models/Waste.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Models/Waste.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Utils/BooleanDialog.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Utils/BooleanDialog.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Utils/IO.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Utils/IO.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/CelebrateView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/CelebrateView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/Command/FlipView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/Command/FlipView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/Command/MoveView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/Command/MoveView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/Command/RedoView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/Command/RedoView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/Command/UndoView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/Command/UndoView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/ConsoleView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/ConsoleView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/EndView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/EndView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/GameView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/GameView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/MenuView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/MenuView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/Models/BoardView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/Models/BoardView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/Models/StackView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/Models/StackView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/Models/SuitView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/Models/SuitView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/StartView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/StartView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/Views/Console/Utils/LimitedIntDialogView.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/Views/Console/Utils/LimitedIntDialogView.cpp.o"
+  "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/src/main.cpp" "/Users/alejandrobadillo/Desktop/ClionWorkspace/Solitario/cmake-build-debug/CMakeFiles/Solitario.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
